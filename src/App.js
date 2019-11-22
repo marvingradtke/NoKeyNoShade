fe59c3a8ea805ba2b6a091a8fbabf0f1abc6662d
marvingradtke <m.gradtke@gmail.com>
2019-11-22 14:59:18 +0100

    added a Logo and expanded some styling attributes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import React from "react";
+import GlobalStyles from "./GlobalStyles";
+import Header from "./components/Header";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <GlobalStyles />
+
+      <Header />
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,22 @@
+import React from "react";
+import styled from "@emotion/styled";
+import logopng from "./assets/NoKeyNoLogo.png";
+
+const AppBar = styled.header`
+  width: 100%;
+  height: 120px;
+  display: flex;
+  background: lightpink;
+`;
+
+const Img = styled.img`
+  width: 30%;
+`;
+
+export default function Header() {
+  return (
+    <AppBar>
+      <Img src={logopng} />
+    </AppBar>
+  );
+}
